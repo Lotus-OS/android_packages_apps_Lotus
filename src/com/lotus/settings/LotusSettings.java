a4eda167fe7deb6f7dcea24a5315959140fc1227
xyyx <xyyx@mail.ru>
2017-11-18 23:24:47 +0800

    LotusOS : Add Oneplus3Parts to main menu

--- src/com/lotus/settings/LotusSettings.java
+++ src/com/lotus/settings/LotusSettings.java
@@ -26,6 +26,7 @@ import android.os.Bundle;
 import android.view.Surface;
 import android.preference.Preference;
 import com.android.settings.R;
+import com.lotus.settings.preferences.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -34,8 +35,15 @@ public class LotusSettings extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        final String KEY_DEVICE_PART = "device_part";
+        final String KEY_DEVICE_PART_PACKAGE_NAME = "org.omnirom.device";
 
         addPreferencesFromResource(R.xml.lotus_settings);
+
+        // DeviceParts
+        if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_PART));
+        }
     }
 
     @Override
